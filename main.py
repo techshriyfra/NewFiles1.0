6630754a6d9ce632a7fa125d621bb35c3c2e54da
techshriyfra <techsashif+eufya@gmail.com>
2025-03-08 17:25:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -10000000000 #Add Force Channel or Db Channel id Try One by One
+pyrogram.utils.MIN_CHANNEL_ID = -1002468318697 #Add Force Channel or Db Channel id Try One by One
 Bot().run()
